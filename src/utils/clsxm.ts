3bd5d04b888fb16bc6e10faf97ca5bf6c70584a1
Den4200 <dennis@dennispham.me>
2022-07-29 18:24:06 -0400

    Add `clsxm` utility for merging and conditional tailwind classes.

---
+++ src/utils/clsxm.ts
@@ -0,0 +1,6 @@
+import clsx, { ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+const clsxm = (...classes: ClassValue[]) => twMerge(clsx(...classes));
+
+export default clsxm;
