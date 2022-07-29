054fa21e77b955e85a6bb8b35844d7a6abcbaeaf
Den4200 <dennis@dennispham.me>
2022-07-29 15:59:25 -0400

    Format code.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,11 @@ export default function Home() {
     <div className="container flex items-center p-4 mx-auto min-h-screen justify-center">
       <main>
         <h1 className="font-mono text-xl code">
-          Welcome to <span className="text-purple-700">Nextjs</span>, <span className="text-indigo-700">TailwindCSS</span> and <span className="text-gray-700">TypeScript</span>.
+          Welcome to <span className="text-purple-700">Nextjs</span>,{" "}
+          <span className="text-indigo-700">TailwindCSS</span> and{" "}
+          <span className="text-gray-700">TypeScript</span>.
         </h1>
       </main>
     </div>
-  )
+  );
 }
