7ce755f6f65129b1a5e4d01c1ddcad4855cf8823
Den4200 <dennis@dennispham.me>
2022-07-29 23:20:36 -0400

    Add live date and time on desktop.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
+import { useEffect, useState } from "react";
 import Bookmarks from "@/components/Bookmarks";
 import Searchbar from "@/components/Searchbar";
 
 const Home = () => {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    setInterval(() => setDate(new Date()), 1000);
+  }, []);
+
   return (
     <main className="mx-auto flex min-h-screen max-w-screen-md flex-col items-center justify-center p-4">
       <div className="w-full space-y-8">
@@ -13,6 +20,10 @@ const Home = () => {
         <Searchbar />
         <Bookmarks />
       </div>
+
+      <p className="absolute bottom-3 right-3 hidden font-semibold text-theme-white sm:block">
+        {date.toLocaleString()}
+      </p>
     </main>
   );
 };
