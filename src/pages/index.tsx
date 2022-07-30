2734b36f2956e05b228ebe1d5b300f32c8c662a5
Den4200 <dennis@dennispham.me>
2022-07-29 21:55:18 -0400

    Decrease max content width.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,13 @@ import Searchbar from "@/components/Searchbar";
 
 const Home = () => {
   return (
-    <main className="container mx-auto flex min-h-screen flex-col items-center justify-center p-4 text-theme-white">
-      <h1 className="mb-8 text-6xl text-theme-beige">
-        &gt; cd ~/
-        <span className="animate-blink">_</span>
-      </h1>
+    <main className="mx-auto flex min-h-screen max-w-screen-md flex-col items-center justify-center p-4">
+      <div className="w-full">
+        <h1 className="mb-8 text-center text-6xl text-theme-beige">
+          &gt; cd ~/
+          <span className="animate-blink">_</span>
+        </h1>
 
-      <div className="w-11/12">
         <Searchbar />
       </div>
     </main>
