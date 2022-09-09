a422ae23a077de4ecbd4634f7edf7e7034c73f48
Dennis Pham <dennis@dennispham.me>
2022-09-09 02:01:13 -0400

    Readd missing bookmarks that were previously removed.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -79,6 +79,39 @@ const BOOKMARKS = [
         name: "tcpshield",
         url: "https://panel.tcpshield.com/",
       },
+      {
+        name: "webshare",
+        url: "https://proxy.webshare.io/dashboard/",
+      },
+      {
+        name: "discord",
+        url: "https://discord.com/developers/",
+      },
+    ],
+  },
+  {
+    name: "docs",
+    links: [
+      {
+        name: "tailwind",
+        url: "https://tailwindcss.com/",
+      },
+      {
+        name: "heroicons",
+        url: "https://heroicons.com/",
+      },
+      {
+        name: "headlessui",
+        url: "https://headlessui.com/",
+      },
+      {
+        name: "kubernetes",
+        url: "https://kubernetes.io/docs/home/",
+      },
+      {
+        name: "mdn",
+        url: "https://developer.mozilla.org/",
+      },
     ],
   },
   {
@@ -89,12 +122,12 @@ const BOOKMARKS = [
         url: "https://aniplix.xyz/",
       },
       {
-        name: "youtube",
-        url: "https://youtube.com/",
-      },
-      {
         name: "netflix",
         url: "https://www.netflix.com/",
+      },
+      {
+        name: "youtube",
+        url: "https://youtube.com/",
       },
     ],
   },
